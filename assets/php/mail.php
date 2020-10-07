431c8603c4021c25058308052c7ddb17c7a900bc
John Andrés Camacho <jcamacho@hifebo.com>
2020-10-07 09:43:04 -0500

    Cambio de user

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -22,7 +22,6 @@ header("Access-Control-Allow-Origin: *");
 
         // Set the email subject.
         $subject = "Gparts - ".$sub;
-
         // Build the email content.
         $email_content = 
         '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">

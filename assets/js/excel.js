e65087c3d2fec016406a6259d63f099299e63396
John Andrés Camacho <jcamacho@hifebo.com>
2020-10-14 18:01:00 -0500

    Implementamos tabs

--- assets/js/excel.js
+++ assets/js/excel.js
@@ -1,3 +1,5 @@
 $.get( "./assets/php/listado.php", ( data ) => {
-    console.log(data);
+    for (const item of data) {
+        $('#'+item.categoria).append('<div class="col"><div class="product-item"><div class="product-item__thumb"><a href="single-product.html"><img class="thumb-primary" src="assets/img/product/product-6.png" alt="Product" /><img class="thumb-secondary" src="assets/img/product/product-7.png" alt="Product" /></a></div><div class="product-item__content mt-3"><h4 class="title"><a href="single-product.html">'+item.nombre+'</a></h4></div></div></div>');
+    }
 });

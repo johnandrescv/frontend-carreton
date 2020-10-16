13c216c1844374fbef3039f48f1f52999730d264
John Andrés Camacho <jcamacho@hifebo.com>
2020-10-16 18:23:26 -0500

    Ultimas mejoras

--- assets/js/excel.js
+++ assets/js/excel.js
@@ -1,9 +1,10 @@
 $.get( "./assets/php/listado.php", ( data ) => {
     let cont = 1;
     for (const item of data) {
-        const element = '<div class="product-item"><div class="product-item__thumb"><a href="detail.html?id='+item.id+'"><img class="thumb-primary" src="./assets/productos/'+cont+'.jpg" alt="Product" /><img class="thumb-secondary" src="./assets/productos/'+cont+'.jpg" alt="Product" /></a></div><div class="product-item__content mt-3"><h4 class="title"><a href="detail.html?id='+item.id+'">'+item.nombre+'</a></h4></div></div>';
+        const element = '<div class="product-item"><div class="product-item__thumb"><a href="detail.html?id='+item.id+'"><img class="thumb-primary" src="./assets/productos/'+cont+'.jpg" alt="Product" /><img class="thumb-secondary" src="./assets/productos/'+cont+'.jpg" alt="Product" /></a></div><div class="product-item__content mt-3"><h4 class="title text-center"><a href="detail.html?id='+item.id+'">'+item.nombre+'</a></h4></div></div>';
+        const fav = '<div class="product-item"><div class="product-item__thumb"><a href="detail.html?id='+item.id+'"><img class="thumb-primary" src="./assets/productos/'+cont+'.jpg" alt="Product" /><img class="thumb-secondary" src="./assets/productos/'+cont+'.jpg" alt="Product" /></a></div><div class="product-item__content mt-3"><div class="ratting"><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span></div><h4 class="title text-center"><a href="detail.html?id='+item.id+'">'+item.nombre+'</a></h4></div></div>';
         if (item.fav === 1) {
-            $('#fav').append(element);
+            $('#fav').append(fav);
         }
         $('#'+item.categoria).append('<div class="col">'+element+'</div>');
         cont++;
@@ -11,34 +12,36 @@ $.get( "./assets/php/listado.php", ( data ) => {
 
     // Best Product Slider Js
     $(".product-carousel").slick({
-        slidesToShow: 5,
+        slidesToShow: 5.2,
         slidesToScroll: 2,
         arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
         responsive: [{
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 1.2,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 2.2,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 3.2,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 1300,
                 settings: {
-                    slidesToShow: 4,
+                    slidesToShow: 4.2,
                     slidesToScroll: 1
                 }
             }

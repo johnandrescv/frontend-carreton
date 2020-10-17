c646e50eec77696f12c37ef34c8cbc1bc38c252a
John Andrés <johnandrescv@gmail.com>
2020-10-16 21:12:31 -0500

    Ultimas mejoras

--- assets/js/excel.js
+++ assets/js/excel.js
@@ -53,3 +53,9 @@ const validateEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+const goSection = (id) => {
+    $('html, body').animate({
+        scrollTop: $("#"+id).offset().top
+    }, 2000)
+};
